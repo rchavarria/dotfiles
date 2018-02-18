d61edfac60c9f3da88a836a944b531223777fbc0
Rubén Chavarría <txingo@gmail.com>
2018-02-18 10:08:28 +0100

    Add script to install Vim plugins

---
+++ vim/install-plugins.sh
@@ -0,0 +1,25 @@
+set STARTING_DIR = $(pwd)
+
+cd ~/.vim/bundle
+
+git clone https://github.com/editorconfig/editorconfig-vim.git
+git clone https://github.com/unblevable/quick-scope
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/leafgarland/typescript-vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/tpope/vim-commentary.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/ryanoasis/vim-devicons
+git clone https://github.com/elixir-lang/vim-elixir.git
+git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/Shougo/vimproc.vim.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/honza/vim-snippets.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/Shougo/unite.vim.git
+git clone https://github.com/Shougo/vimfiler.vim.git
+git clone https://github.com/Shougo/vimshell.vim.git
+
+cd $STARTING_DIR
+
